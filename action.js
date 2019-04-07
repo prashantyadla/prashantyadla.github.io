0774e61bca2cb92f6a6406f0374cca5afabd2218
prashantyadla <prasanthyadla2@gmail.com>
2019-04-07 19:13:18 +0530

    Add fix 2

--- action.js
+++ action.js
@@ -1,3 +1,11 @@
 document.getElementById("hover").onmouseover = function () {
     document.getElementById("hover").style.color = "red";
 }
+
+function mouseOver() {
+    document.getElementById("hover").style.color = "red";
+}
+
+function mouseOut() {
+    document.getElementById("hover").style.color = "black";
+}
