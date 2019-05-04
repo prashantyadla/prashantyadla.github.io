ee87c7d3fa783ab94aee47ef0131b9720db25938
prashantyadla <prasanthyadla2@gmail.com>
2019-05-04 22:26:54 +0530

    basic animation

--- js/action.js
+++ js/action.js
@@ -1,11 +1,22 @@
-// document.getElementById("hover").onmouseover = function () {
-//     document.getElementById("hover").style.color = "red";
-// }
-
 function mouseOver() {
     document.getElementById("hover").style.color = "red";
 }
 
 function mouseOut() {
     document.getElementById("hover").style.color = "black";
+}
+
+function myMove(){
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    var id = setInterval(frame, 10);
+    function frame(){
+        if(pos == 150){
+            clearInterval(id);
+        }else{
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
 }
